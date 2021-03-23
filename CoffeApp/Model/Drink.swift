9c5d8aa2bdd67d0f04a25c540eaba81b1e8f2871
iAnteater <taras.os.92@gmail.com>
2021-03-23 16:04:20 +0200

    initial

--- CoffeApp/Model/Drink.swift
+++ CoffeApp/Model/Drink.swift
@@ -7,14 +7,14 @@
 
 import Foundation
 
-struct Drink: Hashable, Codable, Identifiable{
+struct Drink: Hashable, Codable, Identifiable {
     var id: Int
     var name: String
     var imageName: String
     var category: Category
     var description: String
     
-    enum Category: String, CaseIterable, Codable, Hashable{
+    enum Category: String, CaseIterable, Codable, Hashable {
         case hot = "hot"
         case cold = "cold"
     }
